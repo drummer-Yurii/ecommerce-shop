1d4c50afbd98379c17113eea9143393e500f9e3f
Yurii Rybachok <drummer2009@ukr.net>
2022-12-18 12:31:13 +0200

    add project

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,171 @@
+<template>
+    <header class="header">
+        <div class="header-top">
+            <div class="header-top-left">
+                <div class="header-top-search">
+                    <img src="/svg/header-search.svg" alt="search" class="header-top__icon">
+                </div>
+                <div class="header-top-mobile-menu" @click="isOpenedMobileMenu = !isOpenedMobileMenu">
+                    <img src="/svg/menu.svg" alt="menu" class="header-top__icon">
+                </div>
+            </div>
+            <router-link to="/" class="header-logo">Avion</router-link>
+            <div class="header-top-right">
+                <div class="header-top-right__cart">
+                    <router-link to="/cart">
+                        <img src="/svg/header-cart.svg" alt="cart">
+                    </router-link>
+                </div>
+                <div class="header-top-right__user">
+                    <router-link to="/user">
+                        <img src="/svg/header-user.svg" alt="user">
+                    </router-link>
+                </div>
+            </div>
+        </div>
+        <div class="header-menu">
+            <router-link 
+                class="header-menu__link"
+                :to="element.path" 
+                v-for="(element, i) of menu"
+                :key="i">{{ element.name }}</router-link>
+        </div>
+        <div class="header-menu-mobile" v-if="isOpenedMobileMenu">
+            <router-link 
+                class="header-menu-mobile__link"
+                :to="element.path" 
+                v-for="(element, i) of menu"
+                :key="i">{{ element.name }}
+            </router-link>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const menu = [
+    {
+        name: 'Plant pots',
+        path: '/plant'
+    },
+    {
+        name: 'Ceramics',
+        path: '/ceramics'
+    },
+    {
+        name: 'Tables',
+        path: '/tables'
+    },
+]
+const isOpenedMobileMenu = ref(false)
+</script>
+
+<style lang="scss" scoped>
+.header {
+    height: 132px;
+    background: #fff;
+    position: relative;
+    @media screen and (max-width: 767px) {
+        height: auto;
+    }
+    &-top {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        height: 70px;
+        align-items: center;
+        border-bottom: 1px solid rgba(0, 0, 0, .1);
+        margin: 0 28px;
+        @media screen and (max-width: 767px) {
+            margin: 0;
+            border: none;
+            height: auto;
+            padding: 20px 0;
+            display: flex;
+            justify-content: space-between;
+        }
+        &__icon {
+            display: block;
+        }
+        &-left {
+            display: flex;
+            align-items: center;
+            @media screen and (max-width: 767px) {
+                order: 2;
+            }
+        }
+        &-mobile-menu {
+            display: none;
+            margin-left: 16px;
+            @media screen and (max-width: 767px) {
+                display: block;
+            }
+        }
+        &-search {
+            @media screen and (max-width: 767px) {
+            }
+        }
+        &-right {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        @media screen and (max-width: 767px) {
+            display: none;
+        }
+        &__cart {
+            margin-right: 16px;
+        }
+    }
+    }
+    &-logo {
+        color: #22202e;
+        font-size: 24px;
+        text-decoration: none;
+        font-family: 'Clash Display', sans-serif;
+        text-align: center;
+        @media screen and (max-width: 767px) {
+            order: 1;
+        }
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+    &-menu {
+        height: 62px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        @media screen and (max-width: 767px) {
+            display: none;
+        }
+        &__link {
+            margin: 0 22px;
+            color: var(--gray);
+            text-decoration: none;
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+        &-mobile {
+            position: absolute;
+            background: #fff;
+            padding: 40px 24px;
+            top: 60px;
+            border: 1px solid;
+            width: 100%;
+            padding: 10px;
+            &__link {
+                display: block;
+                margin-bottom: 20px;
+                color: var(--gray);
+                text-decoration: none;
+                &:last-child {
+                    margin-bottom: 0;
+                }
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/UI/Loader.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="loader-container"><div class="loader"></div></div>
+</template>
+
+<style lang="scss" scoped>
+.loader {
+    animation: rotate 1s infinite;
+    height: 50px;
+    width: 50px;
+    margin-bottom: 50px;
+    &-container {
+        display: flex;
+        justify-content: center;
+    }
+}
+
+.loader::before,
+
+.loader::after {
+    border-radius: 50%;
+    content: "";
+    display: block;
+    height: 20px;
+    width: 20px;
+}
+</style>
